87ffab76d6583eb3565da78bc5c5990974f9e8b1
Francisco-Suntaxi07 <stalynfran@hotmail.com>
2023-12-30 18:54:48 -0500

    implementacion de metodos

--- BibliotecaApp/Controllers/PrestamoController.cs
+++ BibliotecaApp/Controllers/PrestamoController.cs
@@ -18,15 +18,75 @@ namespace BibliotecaApp.Controllers.Prestamos
         public async Task<ActionResult> MainPrestamo()
         {
             List<PrestamoModel> prestamosList = await _prestamoService.ListaPrestamos();
-            ViewBag.Message = "Your contact page.";
             return View(prestamosList);
         }
 
-        public async Task<ActionResult> Form()
+        public async Task<ActionResult> BuscarPrestamo(string id)
         {
-            List<PrestamoModel> prestamosList = await _prestamoService.ListaPrestamos();
-            ViewBag.Message = "Your contact page.";
-            return View(prestamosList);
+            PrestamoModel prestamoModel = new PrestamoModel();
+            ViewBag.Acction = "Nuevo Prestamo";
+
+            if (id != null || id != "")
+            {
+                prestamoModel = await _prestamoService.BuscarPrestamo(id);
+                ViewBag.Acction = "Editar Prestamo";
+            }
+
+            return View(prestamoModel);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> GuardarCambios(PrestamoModel prestamoModel)
+        {
+            bool response = false;
+
+            if (prestamoModel.id_prestamo != null || prestamoModel.id_prestamo != "")
+            {
+                response = await _prestamoService.GuardarPrestamo(prestamoModel);
+            }
+            else
+            {
+                response = await _prestamoService.EditarPrestamo(prestamoModel);
+            } 
+            
+            if (response)
+            {
+                return RedirectToAction("MainPrestamo");
+            }
+            else
+            {
+                return RedirectToAction("MainPrestamo");
+            }
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> EliminarPrestamo(string id) 
+        {
+            var response = await _prestamoService.EliminarPrestamo(id);
+            if (response)
+            {
+                return RedirectToAction("MainPrestamo");
+            }
+            else
+            {
+                return RedirectToAction("MainPrestamo");
+            }
+        }
+
+
+        public async Task<ActionResult> GuardarPrestamo(string id)
+        {
+            PrestamoModel prestamoModel = new PrestamoModel();
+            ViewBag.Acction = "Nuevo Prestamo";
+
+            if (id != null || id != "")
+            {
+                prestamoModel = await _prestamoService.BuscarPrestamo(id);
+                ViewBag.Acction = "Editar Prestamo";
+            }
+
+            return View(prestamoModel);
         }
     }
 }
