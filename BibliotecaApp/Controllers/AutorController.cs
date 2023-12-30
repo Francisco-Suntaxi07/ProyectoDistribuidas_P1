59eccc3d57912a08e02be33a12b93bf1a654ceaf
Santiago <santhiir10@gmail.com>
2023-12-30 17:15:45 -0500

    url Autores

--- BibliotecaApp/Controllers/AutorController.cs
+++ BibliotecaApp/Controllers/AutorController.cs
@@ -1,23 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Mvc;
 
 namespace BibliotecaApp.Controllers
 {
-    public class AutorController
+    public class AutorController : Controller
     {
-        [Key]
-        [StringLength(8)]
-        public string id_autor { get; set; }
-
-        [Required]
-        [StringLength(64)]
-        public string nombre_autor { get; set; }
-
-        [Required]
-        [StringLength(16)]
-        public string nacionalidad_autor { get; set; }
+        // GET: Autor
+        public ActionResult MainAutor()
+        {
+            return View();
+        }
     }
 }
