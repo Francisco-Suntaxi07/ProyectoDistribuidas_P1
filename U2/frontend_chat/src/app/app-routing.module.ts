4e9863674d50c862c67fc80137a4b1f976e0df64
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-02-15 18:00:15 -0500

    update app-routing

--- U2/frontend_chat/src/app/app-routing.module.ts
+++ U2/frontend_chat/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ChatComponent } from './components/chat/chat.component';
 import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 
@@ -8,7 +7,6 @@ const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'home/:userId', component: HomeComponent},
-  {path: 'chat/:userId', component: ChatComponent}
 ];
 
 @NgModule({
